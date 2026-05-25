c5b78d2cae6482a6c7724d32603e6c0bc2b9046c
Minh Dang Pham <minh.pham.024@pslib.cz>
2026-05-25 16:14:59 +0200

    20. commit - menu

---
+++ JS/hamburger.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    let hamelmnts = document.querySelectorAll(".hamburger-zone");
+
+    for (const btn of document.querySelectorAll(".hamburger-btn")) {
+
+        btn.addEventListener("click", (e) => {
+
+            e.preventDefault();
+
+            for (const element of hamelmnts) {
+                element.classList.toggle("active");
+            }  
+            
+        });
+    }
+});
